785a95306c87a677b2717af017d989a07ca2c8c2
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-11-22 14:39:25 -0500

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Exercice1Component } from './components/exercice1/exercice1.component';
+import { Exercice2Component } from './components/exercice2/exercice2.component';
+import { Exercice3Component } from './components/exercice3/exercice3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Exercice1Component,
+    Exercice2Component,
+    Exercice3Component
   ],
   imports: [
     BrowserModule,
